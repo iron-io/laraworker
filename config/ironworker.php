d57c8b53edb98794d5256549eb36b501987bfd25
freeman <kononov.dmitriy@gmail.com>
2014-05-08 12:05:08 +0600

    adding set env param to config

--- config/ironworker.php
+++ config/ironworker.php
@@ -3,6 +3,10 @@
 return array(
     'params' => array(
         'stack' => 'php-5.4',
-        'max_concurrency' => 100
+        'max_concurrency' => 100,
+        'set_env' => array(
+            'my_laravel_env' => 'value1',
+            'my_laravel_env2' => ' value2',
+        )
     )
 );
